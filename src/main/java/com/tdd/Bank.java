4abb5b14daaccfb07d6b3645c6fdff1ef7c6dad1
vasanthk87 <vasanth.kumar@zemosolabs.com>
2018-08-28 14:00:24 +0530

    ch12

---
+++ src/main/java/com/tdd/Bank.java
@@ -0,0 +1,8 @@
+package com.tdd;
+
+public class Bank {
+
+    public Money reduce(Expression source, String to){
+            return Money.getDollar(10);
+    }
+}
--- src/main/java/com/tdd/Dollar.java
+++ src/main/java/com/tdd/Dollar.java
@@ -6,7 +6,7 @@ public class Dollar extends Money{
         super(amount,currency);
     }
 
-    
+
 
 
 }
